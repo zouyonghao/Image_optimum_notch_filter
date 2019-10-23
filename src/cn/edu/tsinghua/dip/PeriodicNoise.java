287a8bab92b47397c9c5827f037e13234220fb5c
zouyonghao <yonghaoz1994@gmail.com>
2019-10-24 00:11:32 +0800

    use different periodic func.

--- src/cn/edu/tsinghua/dip/PeriodicNoise.java
+++ src/cn/edu/tsinghua/dip/PeriodicNoise.java
@@ -3,6 +3,6 @@ package cn.edu.tsinghua.dip;
 class PeriodicNoise {
 
     static double getPixel(int i, int j) {
-        return 20 * Math.sin(20 * i) + 20 * Math.sin(20 * j);
+        return 20 * Math.sin(20 * i);
     }
 }
